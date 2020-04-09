caefdc52e6966168987497c4f664982483ba5715
jiyeon <gbko159@naver.com>
2020-04-09 21:54:02 +0900

    메인메소드 작성했습니다.

--- MyApp.java
+++ MyApp.java
@@ -9,6 +9,10 @@ public class MyApp
 {
     String signal = "녹색";
     double speed = 10;
+    
+    public static void main(String[] args){
+        System.out.println();
+    }
     public void RobotWalk()
     {
         if (signal == "녹색");
