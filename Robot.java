00884b52988b959ae5276bb9a632af71eac785f5
areum <rladkfma2030@gmail.com>
2020-04-10 11:44:36 +0900

    메인 메소드와 로봇워크 메소드를 분리하였습니다.

--- Robot.java
+++ Robot.java
@@ -5,13 +5,8 @@
  * @author (2017315002 김아름, 2018315022 이지연)
  * @version (2020.04.09)
  */
-public class MyApp
-{   
-    public static void main(String[] args){
-        String signal = "빨간색";
-        double speed = 10;
-        System.out.println(RobotWalk(signal, speed));
-    }
+public class Robot
+{
     public static double RobotWalk(String signal, double speed)
     {
         if (signal == "녹색");
