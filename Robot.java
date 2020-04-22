d998485d4457c7f3e6ed93ce76f9bf36da7de47e
areum <rladkfma2030@gmail.com>
2020-04-22 18:10:51 +0900

    My App 클래스 삭제 및 Robot 클래스 수정하였습니다.

--- Robot.java
+++ Robot.java
@@ -4,7 +4,7 @@
  * Write a description of class MyApp here.
  *
  * @author (2017315002 김아름, 2018315022 이지연)
- * @version (2020.04.09)
+ * @version (2020.04.22)
  */
 public class Robot 
 {
@@ -13,6 +13,7 @@ public class Robot
     public void RobotWalk(String signal, double speed)
     {
         if (signal.equals("녹색"));
+            //this.speed = speed;
         else if(signal.equals("노란색"))
             this.speed = speed * 1.2;
         else
